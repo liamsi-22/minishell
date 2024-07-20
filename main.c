1b4a7b6f71749088eda2978f5b1948c281182b50
abakhcha <abakhcha@c4r1p3.1337.ma>
2024-07-20 15:11:27 +0100

    new

--- main.c
+++ main.c
@@ -5,30 +5,112 @@
 #include <stdlib.h>
 
 
+
+t_lexer	*ft_lstlast(t_lexer *lst)
+{
+	while (lst != NULL && lst->next != NULL)
+	{
+		lst = lst->next;
+	}
+	return (lst);
+}
+
+void ft_lstadd_back(t_lexer **lst, t_lexer *new)
+{
+	if (!lst || !new)
+		return ;
+	if (*lst)
+    {
+		ft_lstlast(*lst)->next = new;
+        new->prev = ft_lstlast(*lst);
+    }
+	else
+    {
+		*lst = new;
+        new->prev = NULL;
+    }
+}
+
+t_lexer	*ft_lstnew(char *str, int token, int i)
+{
+	t_lexer	*new;
+
+	new = (t_lexer *)malloc(sizeof(t_lexer));
+	if (!new)
+		return (NULL);
+	new->str = str;
+    new->i = i;
+    new->token = token;
+	new->next = NULL;
+	new->prev = NULL;
+	return (new);
+}
+/**************************************************************/
+t_lexer *init_string(char *ptr,int i)
+{
+    t_lexer *lst;
+
+    lst = NULL;
+    if (i == 1)
+        lst = ft_lstnew(ptr,0,i);
+    else
+        ft_lstadd_back(lst,ft_lstnew(ptr,0,i));
+    return lst;
+}
+
+t_lexer *init_token(int var,int i)
+{
+    t_lexer *lst;
+
+    lst = NULL;
+    if (i == 1)
+        lst = ft_lstnew(0,var,i);
+    else
+        ft_lstadd_back(lst,ft_lstnew(0,var,i));
+    return lst;
+}
+
 int check_token_str(char *str)
 {
-    if(str_len(str) == 2)
+    if(strlen(str) == 2)
     {
         if(str[0] == '>' && str[1] == '>')
-            return 3;
+            return GREAT_GREAT;
         else if(str[0] == '<' && str[1] == '<')
-            return 5;
+            return LESS_LESS;
     }
-    else if(str_len(str) == 1)
+    else if(strlen(str) == 1)
     {
-        if(str[0] == "|")
-            return 1;
-        if(str[0] == ">")
-            return 2;
-        if(str[0] == "<")
-            return 4;
+        if(str[0] == '|')
+            return PIPE;
+        if(str[0] == '>')
+            return GREAT;
+        if(str[0] == '<')
+            return LESS;
     }
     return -1;
 }
 
-void lexing()
+void lexing(char **str)
 {
+    int i = 0;
+    int var = 0;
+    t_lexer *tmp;
 
+    tmp = NULL;
+    while(str[i])
+    {
+        if((var = check_token_str(str[i])) == -1)
+           tmp = init_string(str[i],i);
+        else
+           tmp = init_token(var,i);
+        i++;
+    }
+    while (tmp)
+    {
+        printf("%d\n",i);
+        tmp = tmp->next;
+    }
 }
 
 
@@ -50,7 +132,7 @@ int main(int ac, char **av, char **env)
             printf("\nExiting minishell.\n");
             break;
         }
-        char **spt = split(input," ");
+        char **spt = ft_split(input," ");
         lexing(spt);
         add_history(input);
         free(input);
