71cd4d236bcb591a315a89331e4af8304e76db23
abakhcha <abakhcha@c3r5p8.1337.ma>
2024-08-16 15:14:18 +0100

    changed version

--- assistant_function/ft_strtrim.c
+++ assistant_function/ft_strtrim.c
@@ -1,5 +1,42 @@
 #include "../parsing.h"
 
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	srcsize;
+	size_t	i;
+
+	srcsize = ft_strlen(src);
+	i = 0;
+	if (!dst || !src)
+	{
+		return (0);
+	}
+	if (size != 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (srcsize);
+}
+
+char	*ft_strchr(char const *str, int c)
+{
+	while (*str)
+	{
+		if (*str == (char)c)
+			return ((char *)str);
+		str++;
+	}
+	if (*str == (char)c)
+		return ((char *)str);
+	else
+		return (NULL);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
