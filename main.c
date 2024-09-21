da500bd497374843708a38b5b297ecd356f3ff0e
abakhcha <abakhcha@e1r8p2.1337.ma>
2024-09-21 18:18:04 +0100

    fix norminette

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abakhcha <abakhcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/21 18:17:02 by abakhcha          #+#    #+#             */
+/*   Updated: 2024/09/21 18:17:35 by abakhcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "./parsing.h"
 
 int	find_pwd(t_tools *tools)
@@ -8,28 +20,28 @@ int	find_pwd(t_tools *tools)
 	while (tools->env[i])
 	{
 		if (!ft_strncmp(tools->env[i], "PWD=", 4))
-			tools->pwd = ft_substr(tools->env[i],
-					4, ft_strlen(tools->env[i]) - 4);
+			tools->pwd = ft_substr(tools->env[i], 4, ft_strlen(tools->env[i])
+					- 4);
 		if (!ft_strncmp(tools->env[i], "OLDPWD=", 7))
-			tools->old_pwd = ft_substr(tools->env[i],
-					7, ft_strlen(tools->env[i]) - 7);
+			tools->old_pwd = ft_substr(tools->env[i], 7,
+					ft_strlen(tools->env[i]) - 7);
 		i++;
 	}
 	return (1);
 }
 
-int main(int ac,char **av,char **env)
+int	main(int ac, char **av, char **env)
 {
-    t_tools tools;
-	
+	t_tools	tools;
+
 	(void)av;
-    if(ac != 1)
-    {
-        printf("This program does not accept arguments\n");
-        exit(0);
-    }
-    tools.env = ft_arrdup(env);
-    find_pwd(&tools);
-    init_tools(&tools);
-    minishell_loop(&tools);
+	if (ac != 1)
+	{
+		printf("This program does not accept arguments\n");
+		exit(0);
+	}
+	tools.env = ft_arrdup(env);
+	find_pwd(&tools);
+	init_tools(&tools);
+	minishell_loop(&tools);
 }
