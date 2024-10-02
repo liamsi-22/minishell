b4573595306767ef1554e9f86551e121b4732589
ismail <ismailfagh@gmail.com>
2024-10-02 23:58:35 +0100

    new changes on header file

--- parsing.h
+++ parsing.h
@@ -81,11 +81,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char *ft_strtrim(const char *s);
 char *ft_strncpy(char *dest, char *src, int n);
 int	ft_strcmp(const char *s1, const char *s2);
-
-
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
+
+// parser functions
 
 char	*delete_quotes2(char *str, char c);
 int	init_path(t_tools *tools);
@@ -126,52 +126,7 @@ void	*ft_calloc(size_t count, size_t size);
 t_simple_cmds	*creat_newcmd(char **str, int num_redirections, t_lexer *redirections);
 int	add_newredirection(t_lexer *tmp, t_parser_tools *parser_tools);
 
-//executor functions
-
-// int	export_error(char *c);
-// char	*delete_quotes(char *str);
-// int	ft_isdigit(int c);
-// void	ft_simple_cmdsclear(t_simple_cmds **lst);
-// int	executor(t_tools *tools);
-// int	mini_cd(t_tools *tools, t_simple_cmds *simple_cmd);
-// int	mini_echo(t_tools *tools, t_simple_cmds *simple_cmd);
-// int	mini_env(t_tools *tools, t_simple_cmds *simple_cmd);
-// int	mini_exit(t_tools *tools, t_simple_cmds *simple_cmd);
-// int	mini_export(t_tools *tools, t_simple_cmds *simple_cmd);
-// int	mini_pwd(t_tools *tools, t_simple_cmds *simple_cmd);
-// int	mini_unset(t_tools *tools, t_simple_cmds *simple_cmd);
-// int	check_valid_identifier(char c);
-// char	*delete_quotes_value(char *str);
-// size_t	equal_sign(char *str);
-// void	change_path(t_tools *tools);
-// char	*expand_str(t_tools *tools, char *str);
-// char	*ft_itoa(int n);
-// int	cmd_not_found(char *str, int i);
-// int	after_dol_lenght(char *str, int j);
-// int	check_append_outfile(t_lexer *redirections);
-// char	*char_to_str(char c);
-// int	loop_if_dollar_sign(t_tools *tools, char *str, char **tmp, int j);
-// int	question_mark(char **tmp);
-// int	handle_digit(int j, char *str, char **tmp);
-// int	handle_infile(char *file);
-// int	handle_outfile(t_lexer *redirection);
-// int	create_heredoc(t_lexer *heredoc, bool quotes, t_tools *tools, char *file_name);
-// char	*detect_dollar_sign(t_tools *tools, char *str);
-// size_t	dollar_sign(char *str);
-// int	find_cmd(t_simple_cmds *cmd, t_tools *tools);
-// int	check_redirections(t_simple_cmds *cmd);
-// int	ft_heredoc(t_tools *tools, t_lexer *heredoc, char *file_name);
-// char	*generate_heredoc_filename(void);
-// char	**expander(t_tools *tools, char **str);
-// void	handle_cmd(t_simple_cmds *cmd, t_tools *tools);
-// int	handle_heredoc(t_tools *tools, t_simple_cmds *cmd);
-// t_simple_cmds	*call_expander(t_tools *tools, t_simple_cmds *cmd);
-// void	single_cmd(t_simple_cmds *cmd, t_tools *tools);
-// int multiple_cmd(t_tools *tools);
-// int	ft_fork(t_tools *tools, int end[2], int fd_in, t_simple_cmds *cmd);
-// int	check_fd_heredoc(t_tools *tools, int end[2], t_simple_cmds *cmd);
-// int	pipe_wait(int *pid, int amount);
-// void	dup_cmd(t_simple_cmds *cmd, t_tools *tools, int end[2], int fd_in);
+// executor functions
 
 int export_error(char *c);
 char *delete_quotes(char *str);
@@ -216,7 +171,6 @@ void print_lines(int i, char **str, int out);
 int mini_echo(t_tools *tools, t_simple_cmds *simple_cmd);
 int mini_env(t_tools *tools, t_simple_cmds *simple_cmd);
 void	ft_lexerclear(t_lexer **lst);
-// void	ft_simple_cmdsclear(t_simple_cmds **lst);
 void	free_tools(t_tools *tools);
 int	is_str_digit(char *str);
 void	determine_exit_code(char **str);
